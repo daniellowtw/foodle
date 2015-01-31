b8eb77c0ff58bfba7bb868869066bcc7b66f34a7
D Low <daniellowtw@gmail.com>
2015-01-31 02:24:56 +0000

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    API_KEY:"AIzaSyDzyLH-F9MSNiaW6MHsuxkHqEcbIFffFPE",
+    CLIENT_ID:"791383217892-poit5lob1vc5kd29cl53cl8d71t4ffsv.apps.googleusercontent.com",
+    CLIENT_SECRET:"PDshfp85WmMB-ApJdbyx8LQS ",
+    SCOPE: "https://www.googleapis.com/auth/calendar"
+}
